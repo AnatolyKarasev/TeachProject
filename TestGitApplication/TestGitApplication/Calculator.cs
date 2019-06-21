22d0c01324a5e565925e806a3274ebea0d137393
anatoly.app <anatoly.app@gmail.com>
2019-06-21 20:12:14 +0300

    Create Simple Test application

---
+++ TestGitApplication/TestGitApplication/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitApplication
+{
+    interface ICalculator
+    {        
+        int Sum(int a, int b);
+    }
+
+    class Calculator : ICalculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TestGitApplication/TestGitApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator calculator = new Calculator();
+            
+            Console.WriteLine(calculator.Sum(1, 2));
+        }
+    }
+}
